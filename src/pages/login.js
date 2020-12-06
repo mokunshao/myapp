de540566a2b3cd5bf5b8269edbca89cd2758e6f3
mokunshao <mokunshao@foxmail.com>
2020-12-06 17:15:21 +0800

    backup

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,64 @@
+import { Form, Input, Button } from 'antd';
+
+const layout = {
+    labelCol: {
+        span: 3,
+    },
+};
+
 export default () => {
-    return <div>login</div>;
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+        <div style={{ padding: '2em' }}>
+            <div style={{ maxWidth: '500px' }}>
+                <h1>登录</h1>
+                <Form
+                    {...layout}
+                    name="basic"
+                    initialValues={{}}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                >
+                    <Form.Item
+                        label="用户名"
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入用户名',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="密码"
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入密码',
+                            },
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    );
 };
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,3 +1,64 @@
+import { Form, Input, Button } from 'antd';
+
+const layout = {
+    labelCol: {
+        span: 3,
+    },
+};
+
 export default () => {
-    return <div>register</div>;
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+        <div style={{ padding: '2em' }}>
+            <div style={{ maxWidth: '500px' }}>
+                <h1>注册</h1>
+                <Form
+                    {...layout}
+                    name="basic"
+                    initialValues={{}}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                >
+                    <Form.Item
+                        label="用户名"
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入用户名',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="密码"
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入密码',
+                            },
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                            注册
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    );
 };
